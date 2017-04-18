32c0bef066bdbc2149712a3055d3106bfbe5835c
John Clements <clements@racket-lang.org>
2017-04-18 14:04:51 -0700

    WIP

--- explore-2174-1.rkt
+++ explore-2174-1.rkt
@@ -6,17 +6,43 @@
 
 (define conn (make-connection))
 
+(define started-in-2158
+  (query-rows
+   conn
+   (~a "SELECT id"
+       " FROM class_grade"
+       " WHERE subject = 'CPE'"
+       " AND num = '123'"
+       " AND qtr = 2158"
+       ";")))
+
 (define 2158-regular-track
   (query-rows
    conn
-   (~a "SELECT a.student"
+   (~a "SELECT a.id"
        " FROM (class_grade a"
        "       INNER JOIN"
        "       class_grade b"
-       "       ON a.student = b.student)"
+       "       ON a.id = b.id)"
        " WHERE a.subject = 'CPE'"
        " AND a.num = '123'"
        " AND a.qtr = 2158"
        " AND b.subject = 'CPE'"
        " AND b.num = '102'"
        " AND b.qtr = 2164")))
+
+(define 2158-103-takers
+  (query-rows
+   conn
+   (~a "SELECT a.id"
+       " FROM (class_grade a"
+       "       INNER JOIN"
+       "       class_grade b"
+       "       ON a.id = b.id)"
+       " WHERE a.subject = 'CPE'"
+       " AND a.num = '123'"
+       " AND a.qtr = 2158"
+       " AND b.subject = 'CPE'"
+       " AND b.num = '103'"
+       " AND b.qtr = 2164")))
+
